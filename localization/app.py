252e794e072ec4aa75156de976f6303eadf306da
“kelciatkinson” <kelciatkinson@gmail.com>
2025-02-06 09:57:32 -0600

    task 7-8

--- localization/app.py
+++ localization/app.py
@@ -1,13 +1,27 @@
 #!/usr/bin/env python3
-from babel import Babel, _
-"""
-"""
+from flask_babel import Babel, _, get_locale
+from flask import Flask, request
 
+"""
+"""
 app = Flask(__name__)
+
+app.config['BABEL_DEFAULT_LOCALE'] = 'en'
+app.config['BABEL_TRANSLATION_DIRECTORIES'] = './translations'
+
+
 babel = Babel(app)
 
+def get_locale():
+    return request.accept_languages.best_match(['en', 'fr'])
 
-greeting = _("Hello, World!")
+babel.init_app(app, locale_selector=get_locale)
+
+@app.route('/')
+def index():
+    locale = get_locale()
+    return _("Hello, World!")
+
 
 if __name__ == "__main__":
-    app.run
+    app.run(host="0.0.0.0", port=5001)
